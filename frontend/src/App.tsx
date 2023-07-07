a4c0abf973aa561ed303c24ca00b1c368155ca20
Rebeca Gonzalez <bequipedia@gmail.com>
2023-07-07 13:07:31 +0200

    fix event error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,26 @@
 import './App.css'
-import {useState} from "react";
+import {SetStateAction, useState} from "react";
+import * as React from "react";
 
-function App() {
+const App = () => {
     const [inputValue, setInputValue] = useState('');
     const [moves, setMoves] = useState('0:0:N');
 
-    const handleChange = async (event) => {
+    const handleChange = async (event: { target: { value: SetStateAction<string>; }; }) => {
         setInputValue(event.target.value);
-        const response =
-            await fetch("https://d49ut6g7e1.execute-api.eu-west-1.amazonaws.com/serverless_lambda_stage/rover",
-                {body: JSON.stringify({"input": ""}), method: "POST"});
+        const response = await fetch("https://d49ut6g7e1.execute-api.eu-west-1.amazonaws.com/serverless_lambda_stage/rover", {
+            body: JSON.stringify({"input": ""}),
+            method: "POST"
+        });
         const finalRoverPosition = await response.json();
-        setMoves(finalRoverPosition)
+        setMoves(finalRoverPosition);
     }
 
-    return (
-        <>
-            <label>Insert value
-                <input type={"text"} value={inputValue} onChange={handleChange}/>
-            </label>
-            <p>Value: {moves}</p>
-        </>
-    )
+    return <>
+        <input type="text" value={inputValue} onChange={handleChange}></input>
+        <label>Insert value</label>
+        <p>Value: {moves}</p>
+    </>
 }
 
 export default App
